a80a6f935e4c34bafae4f305c42f18ad3471ed45
BillShackleton <billshackleton@live.co.uk>
2020-12-10 17:58:34 +0000

    doors

---
+++ Assets/Scripts/ThirtyUnitInteract.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirtyUnitInteract : MonoBehaviour
+{
+    public float TargetDistance;
+
+    void Update()
+    {
+        RaycastHit Target;
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Target))
+            {
+                TargetDistance = Target.distance;
+                if (TargetDistance <= 30)
+                {
+                    if (Target.collider.gameObject.name == "DoorSwitch")
+                    {
+                        Target.collider.gameObject.GetComponent<DoorSwitch>().Switch();
+                    }
+                    else if (Target.collider.gameObject.name == "GravitySwitch")
+                    {
+                        Target.collider.gameObject.GetComponent<GravitySwitch>().Switch();
+                    }
+                }
+                print(TargetDistance);
+            }
+        }
+    }
+}
