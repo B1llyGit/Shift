8dab04d81577ac014e0f7195c45b7e18fc01f5e3
BillShackleton <billshackleton@live.co.uk>
2021-01-18 12:47:16 +0000

    Teleport Script for Testing

---
+++ Assets/Scripts/TelePortTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelePortTest : MonoBehaviour
+{
+    public GameObject teleportTarget;
+    public GameObject thePlayer;
+    
+    //Plays Clip On Collision with Player
+    private void OnTriggerEnter (Collider other)  
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+    }
+}
