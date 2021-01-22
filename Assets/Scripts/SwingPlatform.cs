69695d3e110928b0dece31cf1ac441ee8c866fae
BillShackleton <billshackleton@live.co.uk>
2021-01-22 16:15:11 +0000

    Physics Platforms
    Playing around with joints and moving platforms
    
    - Springish platform that falls when players on it springs up when they leave
    
    - Swinging platform when if timed correctly gives the player momentum

---
+++ Assets/Scripts/SwingPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingPlatform : MonoBehaviour
+{
+    public GameObject thePlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == thePlayer)
+        {
+            thePlayer.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == thePlayer)
+        {
+            thePlayer.transform.parent = null;
+        }
+    }
+}
