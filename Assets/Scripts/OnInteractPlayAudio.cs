77cacb8c1c4f535f5aa667b26b30f20dbb6c396a
LasagaGreen <LasagaGreen@users.noreply.github.com>
2021-01-29 01:04:38 +0000

    A couple visual and audio changes
    I've added a bit of the UI to the scene, but it looks like we'll need some more buttons and to organise the menu system.
    I have also added a sound effect to the teleporter, v cool.

---
+++ Assets/Scripts/OnInteractPlayAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnInteractPlayAudio : MonoBehaviour
+{
+
+    public AudioSource teleport;
+
+    void Start()
+    {
+        teleport = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            teleport.Play();
+
+            Destroy (collision.gameObject);
+        }
+    }
+}
+
