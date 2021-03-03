b27dda64be24812688085748d95d3ea9af5ec353
Emelgajizo <74659473+Emelgajizo@users.noreply.github.com>
2021-03-03 08:56:16 +0000

    Checkpoints!

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private GameObject Target;
+    public Vector3 newStart;
+    public Quaternion newRotate;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(collider.gameObject.name);
+        if (collider.gameObject.name == "Third Person Player")
+        {
+            Target = collider.gameObject;
+            Debug.Log(Target);
+            Target.GetComponent<ThirdPersonMovement>().StartPosition = newStart;
+            Target.GetComponent<ThirdPersonMovement>().StartRotation = newRotate;
+        }
+    }
+}
