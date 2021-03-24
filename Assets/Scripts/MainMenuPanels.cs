e3d58dec3995618f5e2a139a7c2bcabda7735704
LasagaGreen <74615463+LasagaGreen@users.noreply.github.com>
2021-03-24 01:14:18 +0000

    Start Screen UI
    Used a script to turn panels on and off during the start screen. We need the UI, Settings and quit button added and then this is completely done.
    I have tested it, but unsure if I properly pushed it, Unitys been glitching out for me.

---
+++ Assets/Scripts/MainMenuPanels.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuPanels : MonoBehaviour
+
+{
+
+    //Main Menu Panels
+
+    public GameObject mainPanel;
+    private bool mainPanelOn = true;
+
+    public GameObject optionsPanel;
+    private bool optionsPanelOn = false;
+
+    public GameObject levelPanel;
+    private bool levelPanelOn = false;
+
+
+    private void Start()
+    {
+
+        mainPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        levelPanel.SetActive(false);
+
+    }
+
+
+
+    public void mainToggle()
+    {
+        if (mainPanelOn == false)
+        {
+            mainPanel.SetActive(true);
+        }
+        else
+        {
+            mainPanel.SetActive(false);
+        }
+
+        mainPanelOn = !mainPanelOn;
+
+    }
+
+
+    public void optionsToggle()
+    {
+        if (optionsPanelOn == false)
+        {
+            optionsPanel.SetActive(true);
+        }
+        else
+        {
+            optionsPanel.SetActive(false);
+        }
+
+        optionsPanelOn = !optionsPanelOn;
+
+    }
+
+
+    public void levelToggle()
+    {
+        if (levelPanelOn == false)
+        {
+            levelPanel.SetActive(true);
+        }
+        else
+        {
+            levelPanel.SetActive(false);
+        }
+
+        levelPanelOn = !levelPanelOn;
+
+    }
+
+
+
+
+
+}
